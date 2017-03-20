6d0fb1d9657fe2323c906adea6629742bfe61c69
Henrique Starosky <>
2017-03-20 08:40:09 -0300

    Adicionada biblioteca Gson + Documentos e classe comanda

--- PesquePague/src/pesque_pague/Comanda.java
+++ PesquePague/src/pesque_pague/Comanda.java
@@ -1,7 +1,12 @@
 package pesque_pague;
 
+import com.google.gson.Gson;
+import java.util.ArrayList;
+
 public class Comanda {
 
-    
-    
+    private Gson gson = new Gson();
+
+    private ArrayList<Produto> a = new ArrayList<>();
+
 }
