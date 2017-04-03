81c59d91364f08a17ae7d55c2d9c3f437b8faad6
Werner A. <informatica@narta.blumenau.ifc.edu.br>
2017-04-03 11:22:42 +0000

    So tirei um espaço.

--- PesquePague/src/pesque_pague/Servicos.java
+++ PesquePague/src/pesque_pague/Servicos.java
@@ -1,11 +1,12 @@
 package pesque_pague;
 
 public class Servicos {
-  
-  int tipoServico;
-  float taxaServico = 0.00f;
-  
-  public float retornaServico() {
-    return taxaServico;
-    
+
+    int tipoServico;
+    float taxaServico = 0.00f;
+
+    public float retornaServico() {
+        return taxaServico;
+
+    }
 }
