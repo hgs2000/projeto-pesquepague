6000c382823678bb36cb9f5f45c85722aab507db
Werner A. <informatica@narta.blumenau.ifc.edu.br>
2017-04-10 12:30:26 +0000

    Adicionado muitas linhas (preguiça de especificar) + javadocs

--- PesquePague/src/pesque_pague/Comanda.java
+++ PesquePague/src/pesque_pague/Comanda.java
@@ -1,40 +1,72 @@
 package pesque_pague;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import java.util.ArrayList;
 
 public class Comanda {
 
-    private Gson gson = new GsonBuilder().create();
+    private boolean comandaAberta;
+    private final String master_comanda;
 
-    private ArrayList<Produto> a = new ArrayList<>();
-
-    public void novaComanda() {
-
+    /**
+     * Muda o valor de comandaAberta para aberta (true).
+     */
+    private void novaComanda() {
+        comandaAberta = true;
     }
 
+    /**
+     * Muda o valor de comandaAberta para fechada (false).
+     */
     public void fechaComanda() {
-
+        comandaAberta = false;
     }
 
+    /**
+     *
+     * @param produto
+     * @param codigoSetor
+     */
     public void adicionaProduto(Produto produto, int codigoSetor) {
 
     }
 
+    /**
+     *
+     * @param peixe
+     */
     public void adicionaProduto(Peixe peixe) {
 
     }
 
+    /**
+     *
+     * @param tipoServico
+     * @param codigoSetor
+     */
     public void adicionaServico(int tipoServico, int codigoSetor) {
 
     }
 
-    public Comanda() {
+    /**
+     * Método utilizado para retornar o estado da Comanda.
+     *
+     * @return true quando aberta e false quando fechada.
+     */
+    public boolean getEstadoComanda() {
+        return comandaAberta;
+    }
 
+    /**
+     * Método utilizado para retornar o nome do usuário dono da Comanda.
+     * @return Nome do usuário da Comanda
+     */
+    public String getNomeMaster() {
+        return master_comanda;
+    }
+
+    public Comanda(String nome_master) {
+        novaComanda();
+        this.master_comanda = nome_master;
     }
 
 }
--- PesquePague/src/pesque_pague/Pesque_Pague.java
+++ PesquePague/src/pesque_pague/Pesque_Pague.java
@@ -1,34 +1,43 @@
 package pesque_pague;
 
-import com.google.gson.stream.JsonReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Pesque_Pague {
 
+    private static ArrayList<Comanda> comanda = new ArrayList<>();
+
     public static void main(String[] args) {
         new CriadorDeJSON();
-    }
+        int cont = 0;
+        do {
+            if (cont == 0) {
+                comanda.add(new Comanda("Henrique Starosky"));
+                cont++;
+            } else {
+                int menu;
+                String[] opc = {"Adicionar Comanda", "Fecha Comanda"};
+                menu = JOptionPane.showOptionDialog(null, "Oi", "Blz", JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION, null, opc, opc[0]);
+                switch (menu) {
+                    case 0:
 
-    static void carregaArquivos() {
-
-        File fl = new File("src/pesque_pague/arquivo.json");
-        if (fl.exists()) {
-            System.out.println("foi");
-            try {
-                FileReader fr = new FileReader(fl);
-                JsonReader reader = new JsonReader(fr);
-
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(Pesque_Pague.class.getName()).log(Level.SEVERE, null, ex);
+                        break;
+                    case 1:
+                        String[] listaComandas = new String[comanda.size()];
+                        for (int i = 0; i < comanda.size(); i++) {
+                            listaComandas[i] = "" + (i+1) + " - " + comanda.get(i).getNomeMaster();
+                        }
+                        JOptionPane.showInputDialog(null, "Selecione a comanda a ser apagada:", "Remove Comanda", JOptionPane.PLAIN_MESSAGE, null,listaComandas ,listaComandas[0]);
+                        break;
+                    default:
+                        System.out.println("ok");
+                }
             }
 
-        } else {
-            System.out.println("não foi");
-        }
-
+        } while (true);
     }
 }
+
+// String[] opc = {"Adicionar Comanda"};
+// menu = JOptionPane.showOptionDialog(null, "Oi", "Blz", JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION, null, opc, opc[0]);
