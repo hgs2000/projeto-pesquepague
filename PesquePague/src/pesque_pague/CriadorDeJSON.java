fe465683358d891d324827cd308a29c14bfb8ab9
Henrique Starosky <hgs.true@gmail.com>
2017-04-13 19:39:20 -0300

    Remoção da Biblioteca GSON

--- PesquePague/src/pesque_pague/CriadorDeJSON.java
+++ PesquePague/src/pesque_pague/CriadorDeJSON.java
@@ -1,14 +1,11 @@
 package pesque_pague;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CriadorDeJSON {
 
     //http://stackoverflow.com/questions/11851603/how-to-convert-hashmap-to-json-file
-    
     public CriadorDeJSON() {
         Map<String, Map<String, Float>> valor_json = new HashMap<>();
         valor_json.put("comidas", new HashMap<String, Float>() {
@@ -42,20 +39,5 @@ public class CriadorDeJSON {
             }
         });
 
-        System.out.println(ConvertJsonToObject.toJSON(valor_json));
-    }
-
-}
-
-class ConvertJsonToObject {
-
-    private static Gson gson = new GsonBuilder().create();
-
-    public static final <T> T getFromJSON(String json, Class<T> clazz) {
-        return gson.fromJson(json, clazz);
-    }
-
-    public static final <T> String toJSON(T clazz) {
-        return gson.toJson(clazz);
     }
 }
