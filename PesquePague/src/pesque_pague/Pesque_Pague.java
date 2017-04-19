16c55a878113d018dbf54f23d896ad4e3ba1b1bf
informatica <informatica@artan.blumenau.ifc.edu.br>
2017-04-19 11:41:52 +0000

    Update

--- PesquePague/src/pesque_pague/Pesque_Pague.java
+++ PesquePague/src/pesque_pague/Pesque_Pague.java
@@ -1,6 +1,8 @@
 package pesque_pague;
 
+import java.awt.Color;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -10,16 +12,18 @@ public class Pesque_Pague {
     private static ArrayList<Comanda> comanda = new ArrayList<>();
     final static JFrame frame = new JFrame() {
         {
+            setTitle("Pesque Pague Peixinho Bom");
             setVisible(true);
             setSize(500, 500);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
+            getContentPane().setBackground(Color.decode("0XACE5EE"));
         }
     };
 
     private static String pedeString(String string) {
         String aux = "";
         do {
-            aux = JOptionPane.showInputDialog(frame, string, "PedeString", JOptionPane.PLAIN_MESSAGE);
+            aux = JOptionPane.showInputDialog(frame, string, null, JOptionPane.PLAIN_MESSAGE);
             if (aux == null) {
                 return pedeString(string);
             }
@@ -38,16 +42,17 @@ public class Pesque_Pague {
                 cont++;
             } else {
                 String[] opc = {"Adicionar Comanda", "Adicionar Produtos", "Fecha Comanda"};
-                menu = JOptionPane.showOptionDialog(frame, new JLabel("Menu", JLabel.CENTER), "Pesque-Paque Peixinho Bom", JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION, null, opc, opc[0]);
+                menu = JOptionPane.showOptionDialog(frame, new JLabel("Menu", JLabel.CENTER), null, JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION, null, opc, opc[0]);
                 switch (menu) {
                     case 0:
                         comanda.add(new Comanda(pedeString("Informe o nome que nome do responsável da comanda: ")));
                         break;
                     case 1:
                         String[] listaAdicionaProduto = retornaListaDePessoas();
-                        String aAlterar = (String) JOptionPane.showInputDialog(frame, new JLabel("Selecione a comanda a ser apagada:", JLabel.CENTER), "Remove Comanda", JOptionPane.PLAIN_MESSAGE, null, listaAdicionaProduto, listaAdicionaProduto[0]);
+                        String aAlterar = (String) JOptionPane.showInputDialog(frame, new JLabel("Selecione a comanda a ser apagada:", JLabel.CENTER), "Remover Comanda", JOptionPane.PLAIN_MESSAGE, null, listaAdicionaProduto, listaAdicionaProduto[0]);
                         if (aAlterar.contains(" (fechada)")) {
                             System.out.println("Está fechada ;-;");
+                            JOptionPane.showMessageDialog(frame, "Está fechada", "Erro", JOptionPane.WARNING_MESSAGE);
                             break;
                         } else {
                             System.out.println("Está aberta ÔuÔ");
@@ -56,8 +61,7 @@ public class Pesque_Pague {
                         }
                     case 2:
                         String[] listaRemocao = retornaListaDePessoas();
-                        String aRemover = (String) JOptionPane.showInputDialog(frame, new JLabel("Selecione a comanda a ser apagada:", JLabel.CENTER), "Remove Comanda", JOptionPane.PLAIN_MESSAGE, null, listaRemocao, listaRemocao[0]);
-                        int valorARemover = pegaCodigoComanda(aRemover);
+                        int valorARemover = pegaCodigoComanda((String) JOptionPane.showInputDialog(frame, new JLabel("Selecione a comanda a ser apagada:", JLabel.CENTER), "Remove Comanda", JOptionPane.PLAIN_MESSAGE, null, listaRemocao, listaRemocao[0]));
                         if (listaRemocao[valorARemover - 1].contains(" (fechada)")) {
                             JOptionPane.showMessageDialog(frame, "Está fechada", "Erro", JOptionPane.WARNING_MESSAGE);
                         } else {
